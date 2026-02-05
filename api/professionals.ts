d961f91af2f5c4783c5832f886c77b5289ef8402
GustavoTiburcio <gustavotiburcio23@gmail.com>
2026-02-05 10:17:08 -0300

    feat: POST ap/api/professionals

--- api/professionals.ts
+++ api/professionals.ts
@@ -7,13 +7,10 @@ export default async function handler(
   res: VercelResponse
 ) {
   if (withCors(req, res)) return;
-  
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
 
-  try {
-    const result = await pool.query(`
+  if (req.method === 'GET') {
+    try {
+      const result = await pool.query(`
       SELECT
         id,
         name,
@@ -25,14 +22,72 @@ export default async function handler(
       ORDER BY rating DESC, name
     `);
 
-    const professionals = result.rows.map(p => ({
-      ...p,
-      rating: Number(p.rating),
-    }));
+      const professionals = result.rows.map(p => ({
+        ...p,
+        rating: Number(p.rating),
+      }));
 
-    res.status(200).json(professionals);
-  } catch (error) {
-    console.error('Erro ao buscar profissionais:', error);
-    res.status(500).json({ error: 'Erro ao buscar profissionais' });
+      return res.status(200).json(professionals);
+    } catch (error) {
+      console.error('Erro ao buscar profissionais:', error);
+      res.status(500).json({ error: 'Erro ao buscar profissionais' });
+    }
   }
+
+  if (req.method === 'POST') {
+
+    const {
+      name,
+      avatar,
+      specialties,
+      rating,
+    } = req.body;
+
+    if (
+      !name ||
+      !avatar ||
+      !specialties ||
+      !rating
+    ) {
+      return res.status(400).json({ error: 'Dados obrigatórios não informados' });
+    }
+
+    const client = await pool.connect();
+
+    try {
+      await client.query('BEGIN');
+
+      // 3️⃣ Inserir profissional
+      const insertResult = await client.query(
+        `
+        INSERT INTO professionals
+          (name, avatar, specialties, rating, active)
+        VALUES
+          ($1, $2, $3, $4, $5)
+        RETURNING id, name, avatar, specialties, rating, active
+        `,
+        [
+          name,
+          avatar,
+          specialties,
+          rating,
+          true,
+        ]
+      );
+
+      await client.query('COMMIT');
+
+      return res.status(201).json(insertResult.rows[0]);
+
+    } catch (error) {
+      await client.query('ROLLBACK');
+      console.error(error);
+      return res.status(500).json({ error: 'Erro ao criar agendamento' });
+    } finally {
+      client.release();
+    }
+  }
+
+  return res.status(405).json({ error: 'Method not allowed' });
+
 }
