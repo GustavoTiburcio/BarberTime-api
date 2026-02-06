e2904b0b73c41da2d86568b1a581b769948c3e70
GustavoTiburcio <gustavotiburcio23@gmail.com>
2026-02-06 14:42:14 -0300

    fix: CORS Header Authorization

--- api/_utils/cors.ts
+++ api/_utils/cors.ts
@@ -6,7 +6,7 @@ export function withCors(
 ) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
